ca940c416976af1f87a2dad2ef928b664d3f78d0
Bryce Leonard <bryce.a.leonard@gmail.com>
2016-03-15 18:40:38 -0600

    initial storyboard

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+class Pitchers{
+
+    var firstName:String
+    var lastName:String
+
+    init(firstName:String, lastName:String){
+        self.firstName = firstName
+        self.lastName = lastName
+    }
+    
+    func startTalking() -> String{
+        return "I just started to talk"
+    }
+
+}
+
+
+var people = Pitchers([{firstName;: "bryce", lastName: "leonard"firstName; "bruce";, lastName: "springsteen"])
+
+
